2d65daa5941646d4e4caa01527e3c577675bea46
Caitlin Moreno <crmoreno@usc.edu>
2018-03-30 09:26:38 -0700

    Remove timestamp

--- Login.java
+++ Login.java
@@ -1,31 +1,24 @@
 //import DatabaseInteract
 //import User
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 
 public class Login {
 	String username;
 	String password;
-	String timestamp;
 
 	//deault constructor sets current user to null
 	public Login() {
 		username = null;
 		password = null;
-		timestamp = null;
 	}
 
 	//constructor sets current user to these inputs
 	public Login(String un, String pw, String ts) {
 		username = un;
 		password = pw;
-		//unclear what the timestamp is for
-		timestamp = ts;
 	}
 
 	//returns true if the user is successfully logged in, false if not
 	public Boolean userValidated(String username, String password) {
-		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
 		DatabaseInteract dataInteract = new DataBaseInteract();
 		User[] userArr = dataInteract.getUsers();
 
@@ -51,11 +44,5 @@ public class Login {
 	//sets password
 	public void setPassword(String pw) {
 		password = pw;
-	}
-
-	//sets timestamp
-
-	public void setTimestamp(String ts) {
-		timestamp = ts;
 	}
 }
