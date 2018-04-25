901db0da621d4d3713465e9641e402ee64ebca6f
Caitlin Moreno <crmoreno@usc.edu>
2018-04-24 20:04:11 -0700

    Add defaultLocation SQL File

---
+++ loadDefaultLocations.sql
@@ -0,0 +1,12 @@
+INSERT INTO `FinalProjData`.`Messages` (`messageID`, `text`, `audio`, `image`, `flagCount`, `loc_x`, `loc_y`) VALUES ('2', 'This is cargar', '', '', '0', '34.0259', '-118.287');
+INSERT INTO `FinalProjData`.`Messages` (`messageID`, `text`, `audio`, `image`, `flagCount`, `loc_x`, `loc_y`) VALUES ('3', 'This is Cafe 84', '', '', '0', '34.0246', '-118.288');
+INSERT INTO `FinalProjData`.`Messages` (`messageID`, `text`, `audio`, `image`, `flagCount`, `loc_x`, `loc_y`) VALUES ('4', 'This is EVK', '', '', '0', '34.0213', '-118.282');
+INSERT INTO `FinalProjData`.`Messages` (`messageID`, `text`, `audio`, `image`, `flagCount`, `loc_x`, `loc_y`) VALUES ('5', 'This is Leavey', '', '', '0', '34.0217', '-118.283');
+INSERT INTO `FinalProjData`.`Messages` (`messageID`, `text`, `audio`, `image`, `flagCount`, `loc_x`, `loc_y`) VALUES ('6', 'This is THH #1', '', '', '0', '34.022319', '-118.284410');
+INSERT INTO `FinalProjData`.`Messages` (`messageID`, `text`, `audio`, `image`, `flagCount`, `loc_x`, `loc_y`) VALUES ('6', 'This is THH #2', '', '', '0', '34.022074', '-118.284556');
+INSERT INTO `FinalProjData`.`Messages` (`messageID`, `text`, `audio`, `image`, `flagCount`, `loc_x`, `loc_y`) VALUES ('6', 'This is THH #3', '', '', '0', '34.022264', '-118.284661');
+INSERT INTO `FinalProjData`.`Messages` (`messageID`, `text`, `audio`, `image`, `flagCount`, `loc_x`, `loc_y`) VALUES ('7', 'This is Tommy', '', '', '0', '34.0206', '-118.285');
+INSERT INTO `FinalProjData`.`Messages` (`messageID`, `text`, `audio`, `image`, `flagCount`, `loc_x`, `loc_y`) VALUES ('8', 'This is the bookstore', '', '', '0', '34.0207', '-118.286');
+INSERT INTO `FinalProjData`.`Messages` (`messageID`, `text`, `audio`, `image`, `flagCount`, `loc_x`, `loc_y`) VALUES ('9', 'This is JFF', '', '', '0', '34.0186', '-118.282');
+INSERT INTO `FinalProjData`.`Messages` (`messageID`, `text`, `audio`, `image`, `flagCount`, `loc_x`, `loc_y`) VALUES ('10', 'This is IRC', '', '', '0', '34.0192', '-118.291');
+INSERT INTO `FinalProjData`.`Messages` (`messageID`, `text`, `audio`, `image`, `flagCount`, `loc_x`, `loc_y`) VALUES ('11', 'This is SAL', '', '', '0', '34.0196', '-118.289');
